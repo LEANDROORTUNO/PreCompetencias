0eb2cd38dca1813865089f65e125f1e1eabe5a8c
jos2006 <jos2006@githup.com>
2026-05-20 16:05:22 -0400

    jose

--- CodeForces/jose/HelpfulMaths.java
+++ CodeForces/jose/HelpfulMaths.java
@@ -1,1 +1,9 @@
-HelpfulMaths.java
+import java.util.Scanner;
+public class HelpfulMaths{
+	public static void main (String arg []){
+		Scanner sc = new Scanner (System.in);
+		String suma;
+		suma = sc.next();
+
+	}
+}
