beeab2d8170bac882e6bd63e12805ebc1cc209a9
Leandro Ortuño <menprogramacion@gmail.com>
2026-06-03 16:57:35 -0400

    Ejercicio Marco

---
+++ UAB/marco/EjercicioMarco.java
@@ -0,0 +1,34 @@
+
+public class EjercicioMarco{
+
+	public static int voltear(String res){
+
+		String resp = "";
+		for(int i = res.length() - 1; i >= 0; i--){
+			resp += res.charAt(i); 
+		}
+		return Integer.parseInt(resp);
+	}
+
+	public static int resolver(int[] list1, int[] list2){
+		String res = "";
+		int acarreo, numRes;
+		acarreo = 0;
+		for(int i = list1.length - 1; i >= 0; i--){
+			numRes = list1[i] + list2[i] + acarreo;
+			acarreo = numRes / 10 ;
+			numRes %= 10;
+			res += numRes;
+		}
+
+		return voltear(res);
+
+	}	
+
+	public static void main(String[] args){
+		int[] list1 = {2,5,8};
+		int[] list2 = {3,6,9};
+
+		System.out.println(resolver(list1, list2));
+	}
+}
